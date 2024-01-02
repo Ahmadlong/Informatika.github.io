66a4168d0ba96d7e95223ec4f5d3a2a363f537e0
Ahmad Ridwan <148826264+Ahmadlong@users.noreply.github.com>
2024-01-02 02:39:41 -1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSpecs() {
+    var specsDiv = document.getElementById("specs");
+    if (specsDiv.style.display === "none") {
+        specsDiv.style.display = "block";
+    } else {
+        specsDiv.style.display = "none";
+    }
+}
